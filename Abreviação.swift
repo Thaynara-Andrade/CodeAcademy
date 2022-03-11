e1660244b9658a6e73996c10fb7a307a3c146d59
Thaynara Andrade <81530813+Thaynara-Andrade@users.noreply.github.com>
2022-03-11 10:53:38 -0300

    Add files via upload

---
+++ Abreviação.swift
@@ -0,0 +1,12 @@
+var abbreviation = "EG"
+
+if abbreviation == "EN" {
+     print("English")
+  } else if abbreviation == "ES" {
+     print("Spanish")
+  } else if abbreviation == "ZH" {
+     print("Chinese")
+  } else if abbreviation == "RU" {
+  } else {
+     print("Abbreviation not found")
+}
