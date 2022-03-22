4918d5d9a507651e5d9d7cf72be3bd88341459db
Thaynara Andrade <81530813+Thaynara-Andrade@users.noreply.github.com>
2022-03-22 13:17:13 -0300

    Create Doguinho.swift

---
+++ Doguinho.swift
@@ -0,0 +1,12 @@
+var idadecachorro = 6
+// Esta versão do programa é apenas para cães com mais de 2 anos
+var primeirosanos = 0
+var anosseguintes = 0
+var idadehumana = 0
+// Primeiros anos do doguinho
+primeirosanos = 21
+// Nos anos seguintes
+anosseguintes = (idadecachorro - 2) * 4
+// Sabe a idade humana do doguinho
+idadehumana = primeirosanos + anosseguintes
+print("Meu nome é Doguinho! Au, Au, Eu tenho \(idadehumana) anos na idade humana.")
